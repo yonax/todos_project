7e3aeef7540312f3b9af8840832762bc95fb17e5
yonax <crazy.yonax@gmail.com>
2016-07-29 09:36:22 +0500

    card model, migration

--- back/tasks/admin.py
+++ back/tasks/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Task
+from .models import Card, Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     pass
+
+class TaskInline(admin.TabularInline):
+    model = Task
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    inlines = (TaskInline, )
--- back/tasks/models.py
+++ back/tasks/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class Card(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
+    card = models.ForeignKey(Card, related_name='tasks')
     text = models.CharField(max_length=255)
     done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '"{}" - {}'.format(self.text, 'done' if self.done else 'undone')
