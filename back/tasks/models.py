6087db2ac8c19d79d45b5da3cd6fab0b63b4d90f
yonax <crazy.yonax@gmail.com>
2016-08-02 07:08:43 +0500

    task position field, sql migration

--- back/tasks/models.py
+++ back/tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from positions.fields import PositionField
 
 class Card(models.Model):
     name = models.CharField(max_length=255)
@@ -10,6 +11,10 @@ class Task(models.Model):
     card = models.ForeignKey(Card, related_name='tasks')
     text = models.CharField(max_length=255)
     done = models.BooleanField(default=False)
+    position = PositionField(collection='card', default=-1)
+
+    class Meta:
+        ordering = ('position', )
 
     def __str__(self):
         return '"{}" - {}'.format(self.text, 'done' if self.done else 'undone')
